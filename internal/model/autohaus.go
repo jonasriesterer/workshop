c802015c0c323ed17efd2e4149f306ca761d7579
jonasriesterer <jonasriesterer23@gmail.com>
2026-06-19 16:09:30 +0200

    validation fix

--- internal/model/autohaus.go
+++ internal/model/autohaus.go
@@ -5,7 +5,7 @@ import "time"
 type Autohaus struct {
 	ID              uint      `json:"id"`
 	Name            string    `json:"name"            validate:"required,min=2,max=100"`
-	Username        string    `json:"username"        validate:"required,alphanum,min=3,max=50"`
+	Username        string    `json:"username"        validate:"required,min=3,max=50,excludesall= "`
 	Email           string    `json:"email"           validate:"required,email"`
 	AnzahlFahrzeuge int       `json:"anzahlFahrzeuge" validate:"min=0"`
 	Gruendungsdatum time.Time `json:"gruendungsdatum" validate:"required"`
